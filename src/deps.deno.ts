6123238429d1757faf4212fe28695025542888c0
KnorpelSenf <shtrog@gmail.com>
2021-12-02 12:47:31 +0100

    Update grammY dep

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/grammy@v1.4.0/mod.ts";
+export * from "https://deno.land/x/grammy@v1.4.3/mod.ts";
 export * from "https://cdn.skypack.dev/@grammyjs/types@v2.3.1?dts";
