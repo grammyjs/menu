2753c2fb8c8b90033baae9acde07eb7e2b896740
KnorpelSenf <shtrog@gmail.com>
2021-12-07 14:43:05 +0100

    Align dep versions on Deno and Node

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,2 +1,2 @@
 export * from "https://lib.deno.dev/x/grammy@1.x/mod.ts";
-export * from "https://cdn.skypack.dev/@grammyjs/types@v2.3.1?dts";
+export * from "https://cdn.skypack.dev/@grammyjs/types@v2?dts";
